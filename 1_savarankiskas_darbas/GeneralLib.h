8fcd4764f5952e83c4a4b681bbc5ae0dc96eb407
laurynasstanius <laurynas.stanius@gmail.com>
2021-11-22 23:14:06 +0200

    ADD WIP: Refactoring, spliting student groups only using 2 containers

---
+++ 1_savarankiskas_darbas/GeneralLib.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <time.h>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <deque>
+#include <list>
+
+using namespace std;
